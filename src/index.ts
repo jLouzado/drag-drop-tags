4d07b72936adc95cd21aea4a58ab0fe63ca84d86
Joel Louzado <jlouzado@gmail.com>
2020-09-20 15:20:10 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+
+type State = {
+  tags: Record<string, {groupId: string}>
+}
+
+const state = {
+  tag1: {
+    groupId: 'first'
+  },
+  tag2: {
+    groupId: 'second'
+  }
+}
+
+declare const addToGroup: (tag: string, groupId: string) => State
+declare const removeFromGroup: (tag: string, groupId: string) => State
+
+
+
 document.getElementById("app").innerHTML = `
 <h1>Hello Parcel!</h1>
 <div>
